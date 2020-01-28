44f06089d5ddeaf1eee92b7a277a0101dfe979ca
Christian Tietze <christian.tietze@gmail.com>
2020-01-28 21:05:47 +0100

    add source link when copying

--- content/js/main.js
+++ content/js/main.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('copy', function (event) {
+  var pagelink = '\n\nSource: <a href="' + document.location.href + '">' + document.location.href + '</a>';
+  event.clipboardData.setData('text', document.getSelection() + pagelink);
+  event.preventDefault();
+});
