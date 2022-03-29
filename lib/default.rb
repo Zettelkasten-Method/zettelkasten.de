6730741cb98a30fac359c3704eb2852002db6eaa
Christian Tietze <me@christiantietze.de>
2022-03-29 12:40:13 +0200

    strip HTML tags from item titles

--- lib/default.rb
+++ lib/default.rb
@@ -16,4 +16,22 @@ unless defined? LOADED_DEFAULT_CONFIG
 
   # javascript concatenation
   require 'nanoc/filters/javascript_concatenator'
+
+  def full_page_title
+    item_title = item_title_only
+    result = ""
+    result += item_title + " &bull; " if item_title
+    result += @config[:meta_data][:title]
+    result
+  end
+
+  def item_title_only
+    return nil unless @item[:title]
+    remove_html_tags(@item[:title])
+  end
+
+  def remove_html_tags(string)
+    re = /<("[^"]*"|'[^']*'|[^'">])*>/
+    string.gsub(re, '')
+  end
 end
