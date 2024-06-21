e313785bef4512f56245c94935a5cffff5500be6
Christian Tietze <me@christiantietze.de>
2024-06-21 16:30:02 +0200

    add cache busting with a fingerprint mechanism

---
+++ lib/helpers/fingerprint.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# https://github.com/nanoc/nanoc.app/blob/master/lib/helpers/fingerprint.rb
+def fingerprint(pattern)
+  if ENV['NANOC_ENV'] == 'dev'
+    'dev'
+  else
+    contents = @items.find_all(pattern).map do |i|
+      i.binary? ? File.read(i[:filename]) : i.raw_content
+    end
+    Digest::SHA1.hexdigest(contents.join)[0..15]
+  end
+end
