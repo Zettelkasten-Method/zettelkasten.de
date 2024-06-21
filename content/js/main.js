48527d600a00dc0f6d3b58e9b798c625d23620e1
Christian Tietze <me@christiantietze.de>
2024-06-21 10:08:55 +0200

    add IntersectionObserver-based active ToC highlighting

--- content/js/main.js
+++ content/js/main.js
@@ -33,3 +33,37 @@ ready(function (event) {
           title="See Footnote <% number %>" />(<% number %>)</button>',
   });
 });
+
+ready((event) => {
+  const isActive = (element) => {
+    return element.parentElement.classList.contains('active');
+  };
+
+  let activeElement = null;
+  const setActive = (element) => {
+    if (isActive(element)) { return; }
+
+    if (activeElement) {
+      activeElement.parentElement.classList.remove('active');
+      activeElement = null;
+    }
+
+    element.parentElement.classList.add('active');
+    activeElement = element;
+  };
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      if (entry.isIntersecting) {
+        setActive(document.querySelector(`#toc nav a[href="#${id}"]`));
+      }
+    });
+  });
+
+  document
+    .querySelectorAll('#content h1[id], #content h2[id], #content h3[id], #content h4[id], #content h5[id], #content h6[id]')
+    .forEach((heading) => {
+      observer.observe(heading);
+    });
+});
